67b8b15362c33b03f030125cd65e2a768cc51e68
Nazar <fedunnazar20122001@gmail.com>
2022-12-23 01:38:25 +0200

    make correct paging during searching

--- 04 API_key/js/main.js
+++ 04 API_key/js/main.js
@@ -1,14 +1,20 @@
+//API URL
 const MAIN_URL = `https://thronesapi.com/api/v2/Characters`;
+
+//HTML ELEMENTS
 let row = document.getElementById("root-row");
 let root = document.getElementById("root");
 const searchField = document.getElementById("search");
 const btnNext = document.getElementById("btn-next");
 const btnPrev = document.getElementById("btn-prev");
 const currentPage = document.getElementById("btn-page");
+
+//FOR PAGING
 let onPage = 1;
 let charactersLength = 0;
 var lastId = 20;
 var firstId = 0;
+var searchCharacters = [];
 
 
 window.addEventListener("load", () => {
@@ -20,11 +26,20 @@ searchField.addEventListener("input", () => {
   fetch(MAIN_URL).then(response => {
     return response.json();
   }).then(data => {
+    onPage = 1;
+    firstId = 0;
+    lastId = 20;
     if (text.length == 0) {
-      PrintCharacters(data);
+      //FOR OUTPUTTING ALL CHARACTERS IN ONE TIME
+      // PrintCharacters(data);
+
+      PrintNext20Characters(MAIN_URL, firstId, lastId);
     }
     else {
-      PrintSearchCharacters(data, text.toLowerCase());
+      //FOR OUTPUTTING ALL CHARACTERS IN ONE TIME
+      //PrintSearchCharacters(data, text.toLowerCase());
+
+      PrintNext20SearchCharacters(MAIN_URL, firstId, lastId, text.toLowerCase());
     }
   });
   
@@ -121,6 +136,62 @@ function PrintNext20Characters(URL, begin, end) {
   
 }
 
+function PrintNext20SearchCharacters(URL, begin, end, text) {
+  function SearchCharacters(url) {
+    fetch(url).then(response => {
+      return response.json();
+    }).then(data => {
+      searchCharacters.length = 0;
+      data.forEach(element => {
+        if (element.fullName.toLowerCase().includes(text)) {
+          searchCharacters.push(element);
+          console.log(searchCharacters.length);
+        }
+      })
+    }).then( function() {
+      Print();
+      ChangeColView();
+    }).catch(error => console.log(error))
+    console.log("Search Characters => ", searchCharacters);
+  }
+  SearchCharacters(URL);
+  
+
+  function Print() {
+    row.innerHTML = "";
+    for (let i = begin; i < end; i++) {
+      try {
+        row.innerHTML += `<div class="col-lg-4 col-md-6 col-character">
+        <div class="card mb-3">
+          <h3 class="card-header">${searchCharacters[i].fullName}</h3>
+          <div class="card-header">
+            <h5 class="card-title">${searchCharacters[i].title}</h5>
+            <h6 class="card-subtitle text-muted">${searchCharacters[i].family}</h6>
+          </div>
+          <div class="card-body">
+            <img src="${searchCharacters[i].imageUrl}" alt="${searchCharacters[i].image}" class="img-fluid" id="card-photo">
+          </div>
+          <div class="card-body">
+            <a href="${searchCharacters[i].imageUrl}" target="_blank" class="card-link">Image link</a>
+            <a href="https://thronesapi.com/" target="_blank" class="card-link">API link</a>
+          </div>
+          <div class="card-footer text-muted">
+            Now
+          </div>
+        </div>
+      </div>`;
+      } catch(error) {
+        console.log(error);
+        SetCurrentPage(searchCharacters.length);
+        return;
+      }
+    }
+    SetCurrentPage(searchCharacters.length);
+    console.log("END");
+  }
+  
+}
+
 function PrintSearchCharacters(characters, text) {
   row.innerHTML = "";
   characters.forEach(element => {
@@ -146,10 +217,11 @@ function PrintSearchCharacters(characters, text) {
       </div>`;
     }
   })
-  
-  function ChangeColView() {
-    let colCharacter = document.getElementsByClassName("col-character");
-  console.log(colCharacter);
+  ChangeColView();
+}
+
+function ChangeColView() {
+  let colCharacter = document.getElementsByClassName("col-character");
   if (colCharacter.length == 1) {
     colCharacter[0].setAttribute("class", "col-auto col-character");
     root.style.justifyContent = "center";
@@ -160,8 +232,6 @@ function PrintSearchCharacters(characters, text) {
     }
     root.style.justifyContent = "space-between";
   }
-  }
-  ChangeColView();
 }
 
 function SetCurrentPage(length) {
