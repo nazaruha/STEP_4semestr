6f40ba77780ff25e8ba5cf3bd8eaa7c297c68310
Nazar <fedunnazar20122001@gmail.com>
2023-02-01 12:20:12 +0200

    first MVC project. Work with Controllers and Views

---
+++ FirstRealProject/E_LEARN/E_LEARN.Web/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using E_LEARN.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace E_LEARN.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult HowItWorks()
+        {
+            return View();
+        }
+
+        public IActionResult OrderNow()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Prices()
+        {
+            return View();
+        }
+
+        public IActionResult Discounts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
