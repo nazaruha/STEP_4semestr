5deed3518d4ee081190cfb733b50344abb811fb9
Nazar <fedunnazar20122001@gmail.com>
2023-03-15 17:57:56 +0200

    modify routing

--- HybridProject/FrontEnd/dashboard/src/App.tsx
+++ HybridProject/FrontEnd/dashboard/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
 import DashboardLayout from "./containers/dashboardLayout";
 import DefaultPage from "./pages/defaultPage";
 import Users from "./pages/users";
@@ -8,12 +10,16 @@ import NotFound from "./pages/notFound"
 const App:React.FC = () => {
     return (
         <Routes>
+            {["/", "/sign-in"].map((path) => ( // two paths for one route
+                <Route path={path} element={<SignIn />} />
+            ))}
+            <Route path="/sign-up" element={<SignUp />}/>
             <Route path="/dashboard" element={<DashboardLayout />}>
                 <Route index/* index - батьківський елемент(route) означає той шо на початку буде*/ element={<DefaultPage />} />
                 <Route path="users" element={<Users />}/>
 
             </Route>
-            {/* 👇️ only match this when no other routes match. THIS * is like else in code */}
+            {/* 👇️ only match this when no other routes match. THIS '*' in the 'path' attribute works like 'else' in code */}
             <Route path="*" element={<NotFound />}></Route>
         </Routes>
     )
