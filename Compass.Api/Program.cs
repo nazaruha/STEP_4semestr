b28a818fa6e67fd678c92b4002d4c36ce6c7acf2
macnaer <trofimchuk.an@gmail.com>
2023-03-16 20:55:07 +0200

    Fixed all issues.

--- Compass.Api/Program.cs
+++ Compass.Api/Program.cs
@@ -1,29 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-string connStr = builder.Configuration.GetConnectionString("DefaultConnection");
-// Database context
-builder.Services.AddDbContext(connStr); // Not Working
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using Compass.Infrastructure;
+namespace Compass.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            string connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+            // Database context
+            builder.Services.AddDbContext(connStr);
+            // Add identity
+            builder.Services.AddIdentity();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
--- Compass.Infrastructure/ServiceExtentions.cs
+++ Compass.Infrastructure/ServiceExtentions.cs
@@ -1,5 +1,6 @@
 ﻿using Compass.Core.Entities;
 using Compass.Infrastructure.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -18,34 +19,23 @@ namespace Compass.Infrastructure
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
         }
 
-        //public static void AddIdentity(this IServiceCollection services)
-        //{
-        //    services.AddIdentity<AppUser, IdentityRole>()
-        //        .AddDefaultTokenProviders()
-        //        .AddEntityFrameworkStores<AppDbContext>();
-        //}
-        //public static void AddDbContext(this IServiceCollection services, string connectionString)
-        //{
-        //    services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
-        //}
-
-        //public static void AddInfrastructureServices(this IServiceCollection services)
-        //{
-        //    // Add Identity
-        //    services.AddIdentity<AppUser, IdentityRole>(options =>
-        //    {
-        //        options.SignIn.RequireConfirmedEmail = true;
-        //        options.Lockout.MaxFailedAccessAttempts = 5;
-        //        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-        //        options.Password.RequireDigit = true;
-        //        options.Password.RequireUppercase = true;
-        //        options.Password.RequireLowercase = true;
-        //        options.Password.RequiredLength = 6;
-        //        options.Password.RequireNonAlphanumeric = true;
-        //        options.User.RequireUniqueEmail = true;
-        //    })
-        //                .AddEntityFrameworkStores<AppDbContext>()
-        //                .AddDefaultTokenProviders();
-        //}
+        public static void AddIdentity(this IServiceCollection services)
+        {
+            // Add Identity
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.User.RequireUniqueEmail = true;
+            })
+                        .AddEntityFrameworkStores<AppDbContext>()
+                        .AddDefaultTokenProviders();
+        }
     }
 }
