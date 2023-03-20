1b05460d6fd74a2358930add486eae051c450a02
Nazar <fedunnazar20122001@gmail.com>
2023-03-20 17:24:34 +0200

    was trying to make copyright as a component but there is unknown mistake

--- HybridProject/FrontEnd/dashboard/src/pages/auth/signIn/index.tsx
+++ HybridProject/FrontEnd/dashboard/src/pages/auth/signIn/index.tsx
@@ -14,7 +14,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {Formik, Field} from 'formik';
-import { LoginSchema } from '../validation';
+import { LoginSchema } from '../validation'; 
 
 function Copyright(props: any) {
   return (
