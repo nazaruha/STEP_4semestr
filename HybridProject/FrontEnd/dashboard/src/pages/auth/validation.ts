b7e659107238479ffc80203ba0ed7ccd19f27439
Nazar <fedunnazar20122001@gmail.com>
2023-03-27 15:43:04 +0300

    add confirmPassword validation

--- HybridProject/FrontEnd/dashboard/src/pages/auth/validation.ts
+++ HybridProject/FrontEnd/dashboard/src/pages/auth/validation.ts
@@ -17,19 +17,28 @@ export const LoginSchema = Yup.object().shape({
 
 export const RegisterSchema = Yup.object().shape({
     firstName: Yup.string()
+        .max(255)
         .required("Required")
         .label("First Name"),
     lastName: Yup.string()
+        .max(255)
         .required("Required")
         .label("Last Name"),
     email: Yup.string()
-        .email()
+        .email("Must be a valid email")
+        .max(255)
         .required("Required")
         .label("Email Address"),
     password: Yup.string()
         .required("Required")
+        .max(255, "Maximum password length is 255")
         .min(6, "Password must have at least 6 characters")
         .matches(passwordRegExp, "Password must contain A-Z, a-z, 0-9. Addition can be symbols (#?!@$%^&*0-_)")
         .label("Password"),
-
-})
+    confirmPassword: Yup.string()
+        .required("Required")
+        .max(255)
+        .min(6)
+        .matches(passwordRegExp, "Confirm password must contain A-Z, a-z, 0-9. Addition can be symbols (#?!@$%^&*0-_)")
+        .oneOf([Yup.ref("password")], "Passwords must match.")
+});
